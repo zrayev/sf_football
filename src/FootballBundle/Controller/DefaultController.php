1c947563ab4d0e23619b0ed778722401b30f8737
dmitry <zrayev@mail.ru>
2015-11-22 19:48:51 +0200

    create project architecture

--- src/FootballBundle/Controller/DefaultController.php
+++ src/FootballBundle/Controller/DefaultController.php
@@ -3,17 +3,74 @@
 namespace FootballBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    private $countriesData = [
+        'en' => [
+            'name' => 'England',
+            'text' => 'gfhfgffg',
+            'img'  => 'bundles/football/img/england.png'
+        ],
+        'fr' => [
+            'name' => 'France',
+            'text' => 'gfhfgffjkhg877877787g',
+            'img' => 'bundles/football/img/france.png'
+        ]
+    ];
+
+       private $teamsData = [
+        'en' => [
+            'name' => 'Team England',
+            'players' => [
+                [
+                    'name' => 'Vasia Petrov'
+                ],
+                [
+                    'name' => 'Petro Pavlov'
+                ],
+            ],
+            'coach' =>  [
+                'name' => 'Vasia Petrov'
+            ],
+        ],
+        'fr' => [
+            'player' => 'England',
+            'coach' => 'gfhfgffg',
+        ]
+    ];
+
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @return Response
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return $this->render('FootballBundle:Default:index.html.twig', [
+            'countries' => $this->countriesData,
+        ]);
+    }
+
+    /**
+     * @param $name
+     * @return Response
+     */
+    public function countryAction($name)
+    {
+        return $this->render('FootballBundle:Default:country.html.twig', $this->countriesData[$name]);
+    }
+
+    /**
+     * @param $name
+     * @return Response
+     */
+     public function playerAction($name)
+    {
+        return new Response($name);
+    }
+
+    public function teamAction($name)
+    {
+        return $this->render('FootballBundle:Default:team.html.twig', $this->teamsData[$name]);
     }
 }
