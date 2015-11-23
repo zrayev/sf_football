65cd09a7471aebcda393633bbe4e99ca9be954f7
dmitry <zrayev@mail.ru>
2015-11-23 02:16:36 +0200

    add tests

--- src/FootballBundle/Tests/Controller/DefaultControllerTest.php
+++ src/FootballBundle/Tests/Controller/DefaultControllerTest.php
@@ -14,4 +14,37 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertTrue($crawler->filter('a:contains("England")')->count() == 1);
     }
+
+    public function testCountry()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'country/en');
+        $this->assertCount(1, $crawler->filter('h2'));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testPlayer()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'player/en/1');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+     public function testTeam()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'team/en');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+      public function testCoach()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'coach/en');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
 }
